408a37a808321b017535d9d53e5c47c1cd886734
Yap Zi Ming <zy078@live.mdx.ac.uk>
2021-01-24 10:49:16 +0000

    first version

---
+++ server.js
@@ -0,0 +1,37 @@
+let express = require('express');
+let app = express();
+
+let db;
+let mongoClient = require('mongodb').MongoClient;
+mongoClient.connect('mongodb+srv://ziming99:coolsinxx2@cluster0.e9nwn.mongodb.net/',(err,client)=>{
+    db = client.db('webstore');
+})
+app.use(express.json());
+
+app.param('collectionName',(req,res,next,collectionName)=>{
+    req.collection = db.collection(collectionName);
+    return next();
+})
+
+app.get('/',(req,res,next)=>{
+    res.send("sdda");
+})
+
+app.post('/collection/:collectionName',(res,req,next)=>{
+    req.collecton.insert(req.body,(e,results)=>{
+        if (e) return next (e);
+        res.send(results.ops)
+    })
+})
+app.get('/collection/:collectionName',(req,res,next)=>{
+    req.collection.find({}).toArray((e,results)=>{
+        if(e)return next(e);
+        res.send(results);
+    })
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log("Express server is running at localhost:3000"); 
+})
