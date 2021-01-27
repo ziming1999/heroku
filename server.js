40527fbd074dfc43cef9d0787eb7e3a218e25609
Yap Zi Ming <zy078@live.mdx.ac.uk>
2021-01-27 12:34:57 +0000

    2

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const ObjectID = require('mongodb').ObjectID;
 app.use(function(req,res,next){
 
     res.header("Access-Control-Allow-Headers","*");
+    res.header("Access-Control-Allow-Methods","*");
     res.header("Access-Control-Allow-Origin","*");
     next();
 });
