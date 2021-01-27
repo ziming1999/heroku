113222a05653033b4190ec28e0cea8daa7ed9879
Yap Zi Ming <zy078@live.mdx.ac.uk>
2021-01-27 12:15:32 +0000

    access control allow methods is added

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const ObjectID = require('mongodb').ObjectID;
 app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin","*");
     res.header("Access-Control-Allow-Headers","*");
+    res.header("Access-Control-Allow-Methods","*");
     next();
 });
 
